6f2e0715e736eb9221c4a24de9902b618fbb836c
Marcelo Araújo <santos46ma@gmail.com>
2021-11-01 20:50:34 -0300

    Suite de testes 'AllTests'

--- programa/src/definicoes/CaminhoSaida.java
+++ programa/src/definicoes/CaminhoSaida.java
@@ -1,8 +1,10 @@
 package definicoes;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
@@ -11,6 +13,8 @@ import exceptions.EscritaNaoPermitidaException;
 import main.Uteis;
 
 public class CaminhoSaida {
+	
+	static int counter = 1;
 	
 	private String caminhoSaida = ".\\saida\\";
 	
@@ -33,10 +37,67 @@ public class CaminhoSaida {
 		}
 	}
 	
-	private boolean isValidPath(String path) {
-		// falsificação
-		caminhoSaida = ".\\" + path + "\\";
+	public boolean isValidPath(String path) {
+		File file = new File(path);
+		
+		if(file.isFile()) {
+			return false;
+		}
+		
+		String[] parts = path.split("\\\\");
+		
+		if(!parts[0].equals(".")) {
+			if(parts[0].equals("")) {
+				path = "." + path;
+			}
+			else {
+				path = ".\\" + path;
+			}
+		}
+		
+		System.out.println("final: " + path);
+//		System.out.println("padrão: " + caminhoSaida);
+		
+		
+		if(path.charAt(path.length()-1) != '\\') {
+			path = path + "\\";
+		}
+		path = path + ".placeholder";
+		counter += 1;
+		
+		Path pathToFile = Paths.get(path);
+		File nfile = new File(path);
+		if(nfile.exists()) {
+			return true;
+		}
+		try {
+			Files.createDirectories(pathToFile.getParent());
+			Files.createFile(pathToFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		
 		return true;
+//		return file.isDirectory();
+		
+//		if(!file.isDirectory()) {
+//			return false;
+//		}
+//		
+//		if(file.exists())
+		
+		// falsificação
+		//caminhoSaida = ".\\" + path + "\\";
+		
+//		String novoCaminho = ""+path;
+//		if(path.charAt(path.length()-1) != '\\') {
+//			novoCaminho = novoCaminho + "\\";
+//		}
+
+		
+//		return true;
 //		System.out.println(path.split("\\")[0]);
 //		File f = new File(".\\" + path + "\\placeholder.out");
 //		boolean b = f.canWrite();
