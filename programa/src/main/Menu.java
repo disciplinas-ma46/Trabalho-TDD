8ab5877cf914494c87a1e609810bf1b0a2fc4ef2
Marcelo Araújo <santos46ma@gmail.com>
2021-11-01 23:04:21 -0300

    [Refact] Extrair Método de fazer a escrita dos resultados das análises em arquivos
    Origem: Menu.java
    
    Impactos: Menu.java, EscritorArquivo.java
    
    Explicação: a refatoração foi feita criando um método escreverArquivo na classe Menu.java, com a lógica de escrever o resultado das análises (linha ou coluna) em arquivos

--- programa/src/main/Menu.java
+++ programa/src/main/Menu.java
@@ -2,6 +2,9 @@ package main;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import definicoes.Definicoes;
@@ -14,8 +17,6 @@ import exceptions.EscritaNaoPermitidaException;
 public class Menu {
 	EvolucoesMemory memories = new EvolucoesMemory();
 	EvolucoesTime times = new EvolucoesTime();
-	
-	EscritorArquivo escritor = new EscritorArquivo();
 	
 	Definicoes definicoes = new Definicoes();
 	
@@ -42,9 +43,9 @@ public class Menu {
 
 		definicoes.definirFormatoSaida();
 		
-		escritor.escreverArquivo(memories, definicoes);
+		escreverArquivo(memories, definicoes);
 		
-		escritor.escreverArquivo(times, definicoes);
+		escreverArquivo(times, definicoes);
 		
 		System.out.println("Finalizado");
 		
@@ -84,6 +85,91 @@ public class Menu {
 		    //e.printStackTrace();
 			throw new ArquivoNaoEncontradoException("Arquivo " + nomeArquivo + " não encontrado");
 		}
+	}
+	
+	public void escreverArquivo(Evolucoes evolucoes, Definicoes definicoes) {
+		//definições
+		String caminhoSaida = definicoes.getCaminhoSaida();
+		char delimitador = definicoes.getDelimitador();
+		String formato = definicoes.getFormatoSaida();
+		
+		String caminhoCompleto = caminhoSaida + evolucoes.getNomeSaida();
+		
+		File file = new File(caminhoCompleto);
+		file.getParentFile().mkdirs();
+		FileWriter writer;
+		try {
+			writer = new FileWriter(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		if(formato == "L") {
+			int i = 0;
+			for(ArrayList<Object> evolucao : evolucoes.evolucoes) {
+				try {
+					writer.write("" + (i+1));
+				
+					for(Object valor : evolucao) {
+						writer.write("" + delimitador + valor);
+					}
+					writer.write("\n");
+					i++;
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		if(formato == "C") {
+			try {
+				//int i = 0;
+				int maiorQuantidadeValores = evolucoes.quantidadeValores(0);
+				
+				int qtdEvolucoes = evolucoes.quantidadeEvolucoes();
+			
+				for (int i=0; i<qtdEvolucoes; i++) {
+					int qtdValores = evolucoes.quantidadeValores(i);
+					if(qtdValores > maiorQuantidadeValores) {
+						maiorQuantidadeValores = qtdValores;
+					}
+					writer.write("" + (i+1));
+					if(i != qtdEvolucoes-1) {
+						writer.write("" + delimitador);
+					}
+				}
+				writer.write("\n");
+			
+			
+				for (int j=0; j<maiorQuantidadeValores; j++) {
+					for (int i=0; i<qtdEvolucoes; i++) {
+						Object valor = evolucoes.getValor(i, j);
+						if(valor == null) {
+							writer.write("   ");
+						}
+						else {
+							writer.write("" + valor);
+						}
+						if(i != qtdEvolucoes-1) {
+							writer.write("" + delimitador);
+						}
+					}
+					writer.write("\n");
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		try {
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 	
 }
