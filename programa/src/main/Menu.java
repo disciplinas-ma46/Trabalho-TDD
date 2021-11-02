0fa4ec3e28fb3f03bc796bea9c1a0f957dd7e545
Marcelo Araújo <santos46ma@gmail.com>
2021-11-01 23:18:42 -0300

    [Refact] Substituir método escreverArquivo por objeto-método
    Origem: Menu.java
    
    Impactos: Menu.java, EscreverArquivo.java
    
    Explicação: a refatoração foi aplicada no método escreverArquivo()
    da classe Main. O método foi substituído pela chamada do compute()
    do objeto-método EscreverArquivo.java

--- programa/src/main/Menu.java
+++ programa/src/main/Menu.java
@@ -43,9 +43,9 @@ public class Menu {
 
 		definicoes.definirFormatoSaida();
 		
-		escreverArquivo(memories, definicoes);
+		escreverArquivo(memories);
 		
-		escreverArquivo(times, definicoes);
+		escreverArquivo(times);
 		
 		System.out.println("Finalizado");
 		
@@ -87,88 +87,8 @@ public class Menu {
 		}
 	}
 	
-	public void escreverArquivo(Evolucoes evolucoes, Definicoes definicoes) {
-		//definições
-		String caminhoSaida = definicoes.getCaminhoSaida();
-		char delimitador = definicoes.getDelimitador();
-		String formato = definicoes.getFormatoSaida();
-		
-		String caminhoCompleto = caminhoSaida + evolucoes.getNomeSaida();
-		
-		File file = new File(caminhoCompleto);
-		file.getParentFile().mkdirs();
-		FileWriter writer;
-		try {
-			writer = new FileWriter(file);
-		} catch (IOException e) {
-			e.printStackTrace();
-			return;
-		}
-		
-		if(formato == "L") {
-			int i = 0;
-			for(ArrayList<Object> evolucao : evolucoes.evolucoes) {
-				try {
-					writer.write("" + (i+1));
-				
-					for(Object valor : evolucao) {
-						writer.write("" + delimitador + valor);
-					}
-					writer.write("\n");
-					i++;
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		
-		if(formato == "C") {
-			try {
-				//int i = 0;
-				int maiorQuantidadeValores = evolucoes.quantidadeValores(0);
-				
-				int qtdEvolucoes = evolucoes.quantidadeEvolucoes();
-			
-				for (int i=0; i<qtdEvolucoes; i++) {
-					int qtdValores = evolucoes.quantidadeValores(i);
-					if(qtdValores > maiorQuantidadeValores) {
-						maiorQuantidadeValores = qtdValores;
-					}
-					writer.write("" + (i+1));
-					if(i != qtdEvolucoes-1) {
-						writer.write("" + delimitador);
-					}
-				}
-				writer.write("\n");
-			
-			
-				for (int j=0; j<maiorQuantidadeValores; j++) {
-					for (int i=0; i<qtdEvolucoes; i++) {
-						Object valor = evolucoes.getValor(i, j);
-						if(valor == null) {
-							writer.write("   ");
-						}
-						else {
-							writer.write("" + valor);
-						}
-						if(i != qtdEvolucoes-1) {
-							writer.write("" + delimitador);
-						}
-					}
-					writer.write("\n");
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			
-		}
-		
-		try {
-			writer.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public void escreverArquivo(Evolucoes evolucoes) {
+		new EscreverArquivo(this, evolucoes).compute();
 
 	}
 	
